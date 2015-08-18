65209afb938e59dda76c6cf407b4c5e1182e030e
mbanje <mbanjejvernack@gmail.com>
2015-08-18 13:05:45 +0200

    Add PROJECT_PATH and PROJECT_ROOT variables in settings

--- cholera/settings.py
+++ cholera/settings.py
@@ -15,6 +15,9 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                            os.pardir))
+PROJECT_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, os.pardir))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
