57be174ea11dfc3094ec9acbd5fa000adac7bb3b
mbanje <mbanjejvernack@gmail.com>
2015-08-19 12:12:02 +0200

    Story data sent by RapidPro in a dictionary

--- surveillance_cholera/backend.py
+++ surveillance_cholera/backend.py
@@ -3,5 +3,17 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 @json_view
-def handel_external_request(request):
-	print("You are in the handel_external_request function.")
+def handel_rapidpro_request(request):
+	'''This function receives requests sent by RapidPro.
+	This function send json data to RapidPro as a response.'''
+
+	#We will put all data sent by RapidPro in this variable
+	incoming_data = {}
+
+	#Two couples of variable/value are separated by &
+	#Let's put couples of variable/value in a list called 'list_of_data'
+	list_of_data = request.body.split("&")
+
+	#Let's put all the incoming data in the dictionary 'incoming_data'
+	for couple in list_of_data:
+		incoming_data[couple.split("=")[0]] = couple.split("=")[1]
