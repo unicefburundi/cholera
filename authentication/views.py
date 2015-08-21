3a5661db5fed1079991b338dc538149a718449ee
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2015-08-21 20:41:53 +0200

    adding editing capabilities

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import  DetailView
+from django.views.generic import  DetailView, UpdateView
 from django.contrib.auth import get_user_model
 from authentication.models import UserProfile
+from authentication.forms import UserProfileForm
+from django.core.urlresolvers import reverse
 
 class UserProfileDetailView(DetailView):
     model = get_user_model()
@@ -11,3 +13,14 @@ class UserProfileDetailView(DetailView):
         user = super(UserProfileDetailView, self).get_object(queryset)
         UserProfile.objects.get_or_create(user=user)
         return user
+
+class UserProfileEditView(UpdateView):
+    model = UserProfile
+    form_class = UserProfileForm
+    template_name = "edit_profile.html"
+
+    def get_object(self, queryset=None):
+        return UserProfile.objects.get_or_create(user=self.request.user)[0]
+
+    def get_success_url(self):
+        return reverse("profile", kwargs={'slug': self.request.user})
--- cholera/urls.py
+++ cholera/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from authentication.views import UserProfileDetailView
+from authentication.views import UserProfileDetailView, UserProfileEditView
 
 urlpatterns = patterns('',
     url(r'^$', 'cholera.views.home', name='home'),
