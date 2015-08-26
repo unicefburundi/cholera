75d6dad723686112378bbe36addefbd6039cd96c
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2015-08-26 11:13:59 +0200

    added some layout fixes for displaying a profile user, list of cds

--- surveillance_cholera/urls.py
+++ surveillance_cholera/urls.py
@@ -5,7 +5,8 @@ from .views import *
 urlpatterns = patterns('',
     # dashboard view for viewing all poll reports in one place
     url(r'external_request', handel_rapidpro_request, name="handel_request"),
-    url(r'^cds/', CDSListView.as_view(), name='cds_list'),
+    url(r'^cds/$', CDSListView.as_view(), name='cds_list'),
+    url(r'^cds/(?P<pk>\d+)/$', CDSDetailView.as_view(), name='cds_detail'),
     url(r'^district/', DistrictListView.as_view(), name='district_list'),
     url(r'^province/', ProvinceListView.as_view(), name='province_list'),
     url(r'^patient/', PatientListView.as_view(), name='patient_list'),
--- surveillance_cholera/views.py
+++ surveillance_cholera/views.py
@@ -1,22 +1,27 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from surveillance_cholera.models import CDS, Province, District, Patient, Report
 
 class CDSListView(ListView):
     model = CDS
-    paginate_by = 10  #and that's it !!
+    paginate_by = 10
+
+class CDSDetailView(DetailView):
+    model = CDS
+
+
 
 class ProvinceListView(ListView):
     model = Province
-    paginate_by = 10  #and that's it !!
+    paginate_by = 10
 
 class DistrictListView(ListView):
     model = District
-    paginate_by = 10  #and that's it !!
+    paginate_by = 10
 
 class PatientListView(ListView):
     model = Patient
-    paginate_by = 10  #and that's it !!
+    paginate_by = 10
 
 class ReportListView(ListView):
     model = Report
-    paginate_by = 10  #and that's it !!
+    paginate_by = 10
