605483f826207c7264f1ded6d9c737af359c467d
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2015-08-27 17:02:20 +0200

    added list/detail views, and templates to differents models

--- surveillance_cholera/views.py
+++ surveillance_cholera/views.py
@@ -3,25 +3,35 @@ from surveillance_cholera.models import CDS, Province, District, Patient, Report
 
 class CDSListView(ListView):
     model = CDS
-    paginate_by = 10
+    paginate_by = 25
 
 class CDSDetailView(DetailView):
     model = CDS
 
-
-
 class ProvinceListView(ListView):
     model = Province
-    paginate_by = 10
+    paginate_by = 25
+
+class ProvinceDetailView(DetailView):
+    model = Province
 
 class DistrictListView(ListView):
     model = District
-    paginate_by = 10
+    paginate_by = 25
+
+class DistrictDetailView(DetailView):
+    model = District
 
 class PatientListView(ListView):
     model = Patient
-    paginate_by = 10
+    paginate_by = 25
+
+class PatientDetailView(DetailView):
+    model = Patient
 
 class ReportListView(ListView):
     model = Report
-    paginate_by = 10
+    paginate_by = 25
+
+class ReportDetailView(DetailView):
+    model = Report
