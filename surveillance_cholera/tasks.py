90ac2a84c8cf67498f601178e19a07f9f60c462b
mbanje <mbanjejvernack@gmail.com>
2015-09-01 14:01:38 +0200

    Add column for supervisor on test_temporary.htm template

--- surveillance_cholera/tasks.py
+++ surveillance_cholera/tasks.py
@@ -47,6 +47,7 @@ def ask_update_on_patient(request):
 				an_object['patient_id'] = patient.patient_id
 				an_object['entry_date'] = patient.date_entry
 				an_object['reporter_phone'] = the_reporter_s_phone_number
+				an_object['supervisor_phone'] = the_register.supervisor_phone_number
 				short_patient_id = patient.patient_id[6:]
 				#an_object['message'] = "Vous n avez donne aucune nouvelle sur le patient "+patient.patient_id
 				an_object['message'] = "Vous n avez donne aucune nouvelle sur le patient "+short_patient_id
