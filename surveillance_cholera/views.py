4769ff57c68b3b736eb2120af8d8983d3082faf2
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2015-09-01 06:27:46 +0200

    reorder placeholder of statistics in detail layout

--- surveillance_cholera/views.py
+++ surveillance_cholera/views.py
@@ -8,12 +8,36 @@ from django_tables2 import  RequestConfig
 # CDS              ##
 ###########
 
-def get_data(moh_facility):
+def get_cds_data(moh_facility):
     total ={'total': Patient.objects.filter(cds=moh_facility).count()}
     deces= {'deces' : Patient.objects.filter(cds=moh_facility, intervention='DD').count()}
     sorties = {'sorties' : Patient.objects.filter(cds=moh_facility, intervention='PR').count()}
     hospi = {'hospi' : Patient.objects.filter(cds=moh_facility, intervention='HOSPI').count()}
     nc = {'nc' : Patient.objects.filter(cds=moh_facility, exit_status=None).count()}
+
+    elemet = {}
+    for i in [total,deces,sorties,hospi,nc]:
+            elemet.update(i)
+    return [elemet]
+
+def get_district_data(moh_facility):
+    total ={'total': Patient.objects.filter(cds__district=moh_facility).count()}
+    deces= {'deces' : Patient.objects.filter(cds__district=moh_facility, intervention='DD').count()}
+    sorties = {'sorties' : Patient.objects.filter(cds__district=moh_facility, intervention='PR').count()}
+    hospi = {'hospi' : Patient.objects.filter(cds__district=moh_facility, intervention='HOSPI').count()}
+    nc = {'nc' : Patient.objects.filter(cds__district=moh_facility, exit_status=None).count()}
+
+    elemet = {}
+    for i in [total,deces,sorties,hospi,nc]:
+            elemet.update(i)
+    return [elemet]
+
+def get_province_data(moh_facility):
+    total ={'total': Patient.objects.filter(cds__district__province=moh_facility).count()}
+    deces= {'deces' : Patient.objects.filter(cds__district__province=moh_facility, intervention='DD').count()}
+    sorties = {'sorties' : Patient.objects.filter(cds__district__province=moh_facility, intervention='PR').count()}
+    hospi = {'hospi' : Patient.objects.filter(cds__district__province=moh_facility, intervention='HOSPI').count()}
+    nc = {'nc' : Patient.objects.filter(cds__district__province=moh_facility, exit_status=None).count()}
 
     elemet = {}
     for i in [total,deces,sorties,hospi,nc]:
@@ -55,10 +79,10 @@ class CDSDetailView(DetailView):
         moh_facility = self.kwargs['pk']
         patients = Patient.objects.filter(cds=moh_facility)
         context['patients'] = patients
-        data = get_data(moh_facility)
-        table = PatientTable(data)
-        RequestConfig(self.request).configure(table)
-        context['table'] = table
+        data = get_cds_data(moh_facility)
+        statistics = PatientTable(data)
+        RequestConfig(self.request).configure(statistics)
+        context['statistics'] = statistics
         return context
 
 class ProvinceListView(ListView):
@@ -77,8 +101,13 @@ class ProvinceDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(ProvinceDetailView, self).get_context_data(**kwargs)
-        districts = District.objects.filter(province=self.kwargs['pk'])
+        moh_facility = self.kwargs['pk']
+        districts = District.objects.filter(province=moh_facility)
         context['districts'] = districts
+        data = get_province_data(moh_facility)
+        statistics = PatientTable(data)
+        RequestConfig(self.request).configure(statistics)
+        context['statistics'] = statistics
         return context
 
 class DistrictListView(ListView):
@@ -101,8 +130,13 @@ class DistrictDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(DistrictDetailView, self).get_context_data(**kwargs)
-        cdss = CDS.objects.filter(district=self.kwargs['pk'])
+        moh_facility = self.kwargs['pk']
+        cdss = CDS.objects.filter(district=moh_facility)
         context['cdss'] = cdss
+        data = get_district_data(moh_facility)
+        statistics = PatientTable(data)
+        RequestConfig(self.request).configure(statistics)
+        context['statistics'] = statistics
         return context
 
 class PatientListView(ListView):
