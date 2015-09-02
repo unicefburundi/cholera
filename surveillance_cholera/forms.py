c3027d0b8d071b1a493c8dc91db0f3a13f0c6f97
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2015-09-02 16:05:07 +0200

    removed unecessair files and set the form to work properly

--- surveillance_cholera/forms.py
+++ surveillance_cholera/forms.py
@@ -5,7 +5,6 @@ from surveillance_cholera.models import *
 class SearchForm(forms.Form):
     def __init__(self,  request=None, *args, **kwargs):
         user = None
-        super (SearchForm,self).__init__( *args,**kwargs)
         PROVINCES = Province.objects.values_list('id','name').distinct()
         if request != None and request.user.is_authenticated():
             user = UserProfile.objects.get(user=request.user)
@@ -14,17 +13,17 @@ class SearchForm(forms.Form):
             if level == 'BPS':
                 PROVINCES= Province.objects.filter(code=moh_facility).values_list('id','name').distinct()
                 DISTRICTS = District.objects.filter(province__code=moh_facility).values_list('id','name').distinct()
-                CDSS = CDS.objects.filter(district__province__code=moh_facility).values_list('id','name').distinct()
                 self.base_fields['province'] =  forms.ChoiceField(widget = forms.Select(), choices=PROVINCES)
                 self.base_fields['districts'] = forms.ChoiceField(widget = forms.Select(), choices= [('', '---------')] +[(str(i),n) for i,n in DISTRICTS],  required=False)
                 self.base_fields['cds'] = forms.ChoiceField(widget = forms.Select(), choices= [('', '---------')], required=False)
 
             if level=='BDS':
                 district = District.objects.get(code=moh_facility)
+                CDSS = CDS.objects.filter(district=district).values_list('id','name').distinct()
                 PROVINCES = Province.objects.filter(code=district.province.id).values_list('id','name').distinct()
                 self.base_fields['province'] =  forms.ChoiceField(widget = forms.Select(), choices=PROVINCES)
                 self.base_fields['districts'] = forms.ChoiceField(widget = forms.Select(), choices=[(str(district.id), district.name)],  required=False)
-                self.base_fields['cds'] = forms.ChoiceField(widget = forms.Select(), choices=[('', '---------')], required=False)
+                self.base_fields['cds'] = forms.ChoiceField(widget = forms.Select(), choices=[('', '---------')]+[(str(i),n) for i,n in CDSS], required=False)
 
             if level=='CDS':
                 cds= CDS.objects.get(code=moh_facility)
@@ -38,6 +37,7 @@ class SearchForm(forms.Form):
                 self.base_fields['province'] =  forms.ChoiceField(widget = forms.Select(), choices=PROVINCES)
                 self.base_fields['districts'] = forms.ChoiceField(widget = forms.Select(), choices=[('', '---------')],  required=False)
                 self.base_fields['cds'] = forms.ChoiceField(widget = forms.Select(), choices=[('', '---------')], required=False)
+        super (SearchForm,self).__init__( *args,**kwargs)
 
 
     start_date = forms.DateField(widget=forms.TextInput(attrs={'class':'datePicker'}))
