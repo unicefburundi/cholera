209e7489aac0f054b0ba8bbd11b438e1cf1ea18a
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2015-11-08 16:40:23 +0200

    sending e-mail of activation possible.

--- cholera/settings.py
+++ cholera/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'betterforms',
+    'import_export',
     'surveillance_cholera',
     'django_tables2',
     'djangular',
--- surveillance_cholera/admin.py
+++ surveillance_cholera/admin.py
@@ -1,37 +1,70 @@
 from django.contrib import admin
 from surveillance_cholera.models import  *
-from importcsvadmin.admin import ImportCSVModelAdmin
-from django import forms
+from import_export import resources
+from import_export.admin import ExportMixin
 
-class ProvinceAdminImporter(forms.ModelForm):
+class ProvinceAdminResource(resources.ModelResource):
     class Meta:
         model = Province
         fields = ('name', 'code')
         ordering = ['name']
 
-class ProvinceAdmin(ImportCSVModelAdmin):
-    importer_class = ProvinceAdminImporter
+class ProvinceAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = ProvinceAdminResource
+    list_display = ('name', 'code', )
+    search_fields = ('name', 'code', )
 
-class DistrictAdminImporter(forms.ModelForm):
+class DistrictAdminResource(resources.ModelResource):
     class Meta:
         model = District
-        fields = ('province', 'name', 'code' )
+        fields = ('province__name', 'name', 'code' )
         ordering = ['name']
 
-class DistrictAdmin(ImportCSVModelAdmin):
-    importer_class = DistrictAdminImporter
+class DistrictAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = DistrictAdminResource
+    list_display = ('name', 'code', 'province')
+    search_fields = ('name', 'code', 'province__name')
 
-class CDSAdminImporter(forms.ModelForm):
+    def province(self, obj):
+        return obj.province.name
+
+class CDSAdminResource(resources.ModelResource):
     class Meta:
         model = CDS
-        fields = ('district','name', 'code')
+        fields = ('district__name','name', 'code', 'district__province__name')
         ordering = ['name']
 
-class CDSAdmin(ImportCSVModelAdmin):
-    importer_class = CDSAdminImporter
+class CDSAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = CDSAdminResource
+    list_display = ('name', 'code', 'status', 'district', 'province')
+    search_fields = ('name', 'code', 'district__name', 'district__province__name')
+
+    def district(self, obj):
+        return obj.district.name
+
+    def province(self, obj):
+        return obj.district.province.name
+
+class PatientAdminResource(resources.ModelResource):
+    class Meta:
+        model = Patient
+        fields = ('patient_id', 'colline_name', 'age', 'intervention', 'date_entry', 'exit_status', 'cds__name', 'cds__district__name', 'cds__district__province__name')
+        ordering = ['name']
+
+class PatientAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = PatientAdminResource
+    list_display = ('patient_id', 'colline_name', 'age', 'intervention', 'date_entry', 'exit_status', 'cds', 'district', 'province')
+    list_filter = ('age', 'intervention', 'exit_status')
+    search_fields = ('cds__name', 'patient_id', 'colline_name', 'cds__district__name', 'cds__district__province__name')
+
+    def district(self, obj):
+        return obj.cds.district.name
+
+    def province(self, obj):
+        return obj.cds.district.province.name
 
 admin.site.register(Person)
-admin.site.register(Patient)
+admin.site.register(Patient, PatientAdmin)
 admin.site.register(Province, ProvinceAdmin)
 admin.site.register(District, DistrictAdmin)
 admin.site.register(CDS, CDSAdmin)
