b73ec08a2acfe8b3803c5e3911e6a1f790ea9a0e
mbanje <mbanjejvernack@gmail.com>
2016-08-02 10:17:14 +0200

    Add patient id in the message sent to supervisors

--- surveillance_cholera/recorders.py
+++ surveillance_cholera/recorders.py
@@ -477,7 +477,7 @@ def record_patient(args):
 
 	args['valide'] = True
 	args['info_to_contact'] = "Ce patient a ete bien enregistre avec l identifiant : "+id_patient+". Merci."
-	args['info_to_supervisors'] = "Un nouveau cas de cholera vient d etre signale au centre de sante "+cds_name+", district "+district_name+", province "+province_name
+	args['info_to_supervisors'] = "Un nouveau cas de cholera vient d etre signale au centre de sante "+cds_name+", district "+district_name+", province "+province_name+". Le id de ce nouveau cas est "+id_patient
 	
 
 
